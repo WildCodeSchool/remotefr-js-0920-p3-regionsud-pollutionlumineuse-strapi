d3308fbda93edd7274a270473cac0b69b26f6cec
Benoît Hubert <benoithubert@gmail.com>
2021-01-16 14:07:24 +0100

    Lifecycle hooks to generate summary for key numbers

--- api/keynumber/models/keynumber.js
+++ api/keynumber/models/keynumber.js
@@ -1,8 +1,20 @@
 'use strict';
+const generateSummary = require('../../../helpers/generate-summary')
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate(data) {
+      data.summary = generateSummary(data.text);
+    },
+    beforeUpdate(params, data) {
+      console.log(data)
+      data.summary = generateSummary(data.text);
+      console.log(data)
+    },
+  },
+};
